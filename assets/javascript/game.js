a9a789ca31137a56ee30f288008c8e2f5a75ca3d
Daniel Doyle <dbdoyle182@gmail.com>
2018-03-03 11:19:04 -0500

    vanilla html and light script

---
+++ assets/javascript/game.js
@@ -0,0 +1,139 @@
+// Trivia questions object array
+
+questions = [{
+    q1: [{
+        a1: {
+            answer: "This is the correct answer",
+            value: true,
+        },
+        a2: {
+            answer: "This is the second answer",
+            value: false,
+        },
+        a3: {
+            answer: "This is the third answer",
+            value: false,
+        },
+        a4: {
+            answer: "This is the last answer",
+            value: false,
+        }
+    }],
+    q2: [{
+        a1: {
+            answer: "This is the correct answer2",
+            value: true,
+        },
+        a2: {
+            answer: "This is the second answer",
+            value: false,
+        },
+        a3: {
+            answer: "This is the third answer",
+            value: false,
+        },
+        a4: {
+            answer: "This is the last answer",
+            value: false,
+        }
+    }],
+    q3: [{
+        a1: {
+            answer: "This is the correct answer3",
+            value: true,
+        },
+        a2: {
+            answer: "This is the second answer",
+            value: false,
+        },
+        a3: {
+            answer: "This is the third answer",
+            value: false,
+        },
+        a4: {
+            answer: "This is the last answer",
+            value: false,
+        }
+    }],
+    q4: [{
+        a1: {
+            answer: "This is the correct answer4",
+            value: true,
+        },
+        a2: {
+            answer: "This is the second answer",
+            value: false,
+        },
+        a3: {
+            answer: "This is the third answer",
+            value: false,
+        },
+        a4: {
+            answer: "This is the last answer",
+            value: false,
+        }
+    }],
+    q5: [{
+        a1: {
+            answer: "This is the correct answer5",
+            value: true,
+        },
+        a2: {
+            answer: "This is the second answer",
+            value: false,
+        },
+        a3: {
+            answer: "This is the third answer",
+            value: false,
+        },
+        a4: {
+            answer: "This is the last answer",
+            value: false,
+        }
+    }],
+    q6: [{
+        a1: {
+            answer: "This is the correct answer6",
+            value: true,
+        },
+        a2: {
+            answer: "This is the second answer",
+            value: false,
+        },
+        a3: {
+            answer: "This is the third answer",
+            value: false,
+        },
+        a4: {
+            answer: "This is the last answer",
+            value: false,
+        }
+    }],
+    q7: [{
+        a1: {
+            answer: "This is the correct answer7",
+            value: true,
+        },
+        a2: {
+            answer: "This is the second answer",
+            value: false,
+        },
+        a3: {
+            answer: "This is the third answer",
+            value: false,
+        },
+        a4: {
+            answer: "This is the last answer",
+            value: false,
+        }
+    }],
+}];
+
+// Function that determines the question that appears
+
+var questionFunc = function () {
+    
+}
+
+// Function that displays the array in the game id 
+
